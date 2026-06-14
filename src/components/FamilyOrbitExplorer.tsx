68363506e5ff6c20bda831a4a3b17b562d671308
zy0x1337 <zy0x@web.de>
2026-06-14 15:41:52 +0200

    fix: remove unused useEffect and familyGenus imports

--- src/components/FamilyOrbitExplorer.tsx
+++ src/components/FamilyOrbitExplorer.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "motion/react";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { familyColors, familyGenus } from "../lib/constants";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { familyColors } from "../lib/constants";
 import type { Strain } from "../types/strain";
 import { StrainRail } from "./StrainRail";
 import { ShrimpLogoMark } from "./ShrimpLogoMark";
